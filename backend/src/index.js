1383a56528c8024331490c95044abe0f7b94438b
Abhishek11815100 <abhiyadav5100@gmail.com>
2025-01-23 16:33:47 +0530

    Ready to deploy

---
+++ backend/src/index.js
@@ -0,0 +1,42 @@
+
+import dotenv from "dotenv"
+import { connectDB } from "./lib/db.js";
+import authRoutes from "./routes/auth.route.js"
+import messageRoutes from "./routes/message.route.js"
+import cookieParser from "cookie-parser"
+import bodyParser from "body-parser"
+import cors from "cors"
+import path from "path";
+
+import {app,server} from "./lib/socket.js"
+
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }))
+// parse application/json
+app.use(bodyParser.json())
+app.use(cookieParser())
+app.use(cors({
+    origin: "http://localhost:5173",
+    credentials: true
+}))
+
+const __dirname = path.resolve();
+
+dotenv.config()
+
+
+app.use("/api/auth", authRoutes);
+app.use("/api/messages", messageRoutes);
+
+if(process.env.NODE_ENV === "production") {
+    app.use(express.static(path.join(__dirname, "../frontend/dist")));
+
+    app.get("*", (req,res)=>{
+        res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+    });
+}
+
+server.listen(PORT, ()=>{
+    console.log("hello there: "+ PORT)
+    connectDB();
+})
